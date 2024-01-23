de9b68a4ac39456503bcf9c38e3ae9840f84b11f
Mirko <vistamirko@gmail.com>
2024-01-23 15:31:48 +0100

    aggiorno server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
-// server/server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const salvaDati = require('./salvaDati'); // Importa la funzione di gestione della richiesta
+const fs = require('fs');
 
 const app = express();
+const port = 5001;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/api/salvaDati', salvaDati); // Utilizza la funzione di gestione della richiesta
+// Aggiunta di una logica per creare 'data.json' se non esiste
+const dataFilePath = 'data.json';
+if (!fs.existsSync(dataFilePath)) {
+  fs.writeFileSync(dataFilePath, '[]', 'utf-8');
+}
 
-module.exports = app;
+app.post('/api/salvaDati', (req, res) => {
+  const data = req.body;
+
+  // Leggi i dati attuali dal file JSON
+  const existingData = JSON.parse(fs.readFileSync(dataFilePath, 'utf-8'));
+
+  // Aggiungi i nuovi dati
+  existingData.push(data);
+
+  // Scrivi i dati aggiornati sul file JSON
+  fs.writeFileSync(dataFilePath, JSON.stringify(existingData));
+
+  res.json({ message: 'Dati salvati con successo.' });
+});
+
+app.listen(port, () => {
+  console.log(`Il server è in ascolto sulla porta ${port}`);
+});
